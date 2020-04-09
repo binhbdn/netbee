6e8ec530dab219bee1bace94399a83343eeeeceb
luong nguyenkim <nguyenkimluong97@gmail.com>
2020-04-09 15:06:04 +0700

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,26 @@
 <template>
-
   <div>
-    <navbar></navbar>
-    <nuxt/>
-
-</div>
-
+    <Header></Header>
+    <nuxt />
+  </div>
 </template>
 
 <script>
-  import Navbar from '../components/Navbar'
+import Header from "../components/Header";
 
-  export default {
-    data() {
-      return {
-     
-      }
-    },
-    components: {
-      Navbar
-    },
-    mounted() {
-
-    }
-  }
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header
+  },
+  mounted() {}
+};
 </script>
 
 <style>
-  html {
-    font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
-  }
+html {
+  font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ module.exports = {
       { rel: 'stylesheet', href: '/app-assets/css/plugins/forms/validation/form-validation.css' },
       { rel: 'stylesheet', href: '/assets/css/style.css' },
       { rel: 'stylesheet', href: '/assets/css/custom-client.css' },
+      { rel: 'stylesheet', href: '/assets/css/header.css' },
       { rel: 'stylesheet', href: '/app-assets/css/pages/dashboard-analytics.css' },
       { rel: 'stylesheet', href: '/app-assets/css/pages/card-analytics.css' },
     ],
