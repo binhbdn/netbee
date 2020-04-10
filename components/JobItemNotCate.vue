b501a24708eade6012cd9d2525afe3c2002a4026
Pham Vu Phuong <phamvuphuong98@gmail.com>
2020-04-10 10:38:07 +0700

    add tin tuyen sinh

---
+++ components/JobItemNotCate.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card ecommerce-card border-job-item" style="height: 100px;margin-bottom: 15px;">
+        <div class="card-content">
+            <div class="remove-border-right" style="width: 60vh;padding: 5px 25px;" data-toggle="tooltip" data-placement="top" title="Đăng ký nhận bản tin du học sad ád ád ád ád ád ád ád sa fsdfsf sdfsdfs Cộng đồng chuyên viên tuyển sinh lớn nhất">
+                <a class="item-vip-a" href="#">{{ title }}</a>
+            </div>
+            <div class="item-options text-center">
+                <div class="item-wrapper" style="position: unset">
+                    <div class="item-rating job-vip">
+                        <i class="fas fa-star" style="color: yellow;position: absolute;right: 10px;top: -40px;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-content" style="grid-template-columns: 3fr 6fr 1fr;">
+            <div style="margin: 0 auto;">
+                <a href="app-ecommerce-details.html">
+                    <img src="/app-assets/images/pages/eCommerce/9.png" alt="img-placeholder" width="55" height="55">
+                </a>
+            </div>
+            <div class="remove-border-right">
+                <div class="item-name"  data-toggle="tooltip" data-placement="right" title="Công ty Hachinet">
+                    <p class="item-company mb-0"><i class="fad fa-building"></i> <span class="company-name"> Hachinet</span></p>
+                </div>
+                <div class="item-quantity d-flex justify-content-between">
+                    <p class="quantity-title mb-0"><i class="fad fa-location-arrow"></i> Nhật bản</p>
+                    <p class="delivery-date mb-0"><i class="fad fa-calendar-star"></i> 22/02/2021</p>
+                </div>
+                <div class="item-quantity ">
+                    <p class="delivery-date"><i class="fad fa-funnel-dollar"></i> 1.000.000đ ~ 20.000.000d</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    head: {
+        link: [
+            { rel: 'stylesheet', href: '/app-assets/css/pages/app-ecommerce-shop.css' },
+        ],
+    },
+    props: ['id','title']
+}
+</script>
--- pages/tin-tuyen-sinh.vue
+++ pages/tin-tuyen-sinh.vue
@@ -3,8 +3,8 @@
     <!-- VIỆC LÀM HẤP DẪN -->
     <section>
       <div class="row">
-        <div class="col-lg-12 col-12">
-          <div class="card">
+        <div class="col-lg-8 col-12">
+          <div class="card scrollbar">
             <div class="card-header">
               <h4 class="card-title"><i class="fad fa-bullhorn"></i> VIỆC LÀM HẤP DẪN </h4>
               <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
@@ -18,7 +18,92 @@
             </div>
             <div class="card-content collapse show">
               <div class="card-body" style="padding-bottom: 5px;">
-                <JobsList2Col :DataList="arrayJob"></JobsList2Col>
+                <JobsList1Col :DataList="arrayJob"></JobsList1Col>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-12">
+          <div class="card scrollbar">
+            <div class="card-header">
+              <h4 class="card-title"><i class="fad fa-bullhorn"></i> VIỆC LÀM HOT NHẤT </h4>
+              <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="feather icon-chevron-down"></i></a></li>
+                  <li><a data-action="expand"><i class="feather icon-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="feather icon-x"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-content collapse show">
+              <div class="card-body" style="padding-bottom: 5px;">
+                <JobsList1ColNotCate :DataList="arrayJob"></JobsList1ColNotCate>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- VIỆC LÀM mới -->
+    <section>
+      <div class="row">
+        <div class="col-lg-4 col-12">
+          <div class="card scrollbar">
+            <div class="card-header">
+              <h4 class="card-title"><i class="fad fa-bullhorn"></i> VIỆC LÀM MỚI </h4>
+              <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="feather icon-chevron-down"></i></a></li>
+                  <li><a data-action="expand"><i class="feather icon-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="feather icon-x"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-content collapse show">
+              <div class="card-body" style="padding-bottom: 5px;">
+                <JobsList1ColNotCate :DataList="arrayJob"></JobsList1ColNotCate>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-12">
+          <div class="card scrollbar">
+            <div class="card-header">
+              <h4 class="card-title"><i class="fad fa-bullhorn"></i> VIỆC BÁN THỜI GIAN </h4>
+              <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="feather icon-chevron-down"></i></a></li>
+                  <li><a data-action="expand"><i class="feather icon-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="feather icon-x"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-content collapse show">
+              <div class="card-body" style="padding-bottom: 5px;">
+                <JobsList1ColNotCate :DataList="arrayJob"></JobsList1ColNotCate>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-12">
+          <div class="card scrollbar">
+            <div class="card-header">
+              <h4 class="card-title"><i class="fad fa-bullhorn"></i> THỰC TẬP SINH </h4>
+              <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="feather icon-chevron-down"></i></a></li>
+                  <li><a data-action="expand"><i class="feather icon-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="feather icon-x"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-content collapse show">
+              <div class="card-body" style="padding-bottom: 5px;">
+                <JobsList1ColNotCate :DataList="arrayJob"></JobsList1ColNotCate>
               </div>
             </div>
           </div>
@@ -28,7 +113,24 @@
   </div>
 </template>
 <script>
-export default {
-
-}
+  import JobsList1Col from '~/components/Jobs/JobsList1Col';
+  import JobsList1ColNotCate from '~/components/Jobs/JobsList1ColNotCate';
+  export default {
+    components: {
+      JobsList1Col,
+      JobsList1ColNotCate
+    },
+    data () {
+      return {
+        arrayJob: [
+          {id: 1, title: 'chào cả nhà1'},
+          {id: 2, title: 'chào cả nhà2'},
+          {id: 3, title: 'chào cả nhà3'},
+          {id: 4, title: 'chào cả nhà4'},
+          {id: 5, title: 'chào cả nhà5'},
+          {id: 6, title: 'chào cả nhà6'},
+        ],
+      }
+    },
+  }
 </script>
