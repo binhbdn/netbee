a52a79aa8001e65ef427336de891229a8998f369
Đạt Đặng Tuấn <datdt@hachinet.vn>
2020-04-13 11:18:17 +0700

    update form liên hệ, khoảng cách giữa các khối

--- components/Follow.vue
+++ components/Follow.vue
@@ -1,24 +1,34 @@
 <template>
     <div class="ftco-section-parallax">
-        <div class="parallax-img d-flex align-items-center" id="follow">
+        <div class="d-flex " id="follow">
             <div class="container">
-                <div class="row d-flex justify-content-center">
-                    <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
-                        <h2>Đăng ký nhận bản tin du học</h2>
-                        <p>Hãy để lại thông tin để nhận bản tin du học hoàn toàn miễn phí.</p>
-                        <div class="row d-flex justify-content-center my-3">
-                            <div class="col-md-12 col-xl-8 col-12 p-xl-0">
-                                <form action="#" class="subscribe-form d-inline-block" >
-                                    <div class="form-group d-flex">
-                                        <input type="text" style="border-color: #fff !important;" class="form-control" placeholder="Địa chỉ Email">
-
-                                        <button class="submit px-3">
-                                            <i class="fa fa-plane"></i> Đăng ký
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-7">
+                        <div class="register_form">
+                            <div><h2 class="text">Đăng ký tư vấn</h2></div>
+                            <p>Hãy để lại thông tin để nhận tư vấn từ netbee.</p>
+                            <form action="" method="">
+                                <div class="row">
+                                    <div class="form-group col-sm-6">
+                                        <input type="text" placeholder="Nhập họ và tên" class="form-control" name="name" required>
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                        <input type="text" placeholder="Nhập số điện thoại" class="form-control" name="phone" required>
+                                    </div>
+                                    <div class="form-group col-sm-12">
+                                        <input type="email" placeholder="Nhập địa chỉ email" class="form-control" name="email" required>
+                                    </div>
+                                    <div class="col-sm-12 text-center form-group">
+                                        <button class="btn btn-warning text-dark w-100">
+                                            <i class="far fa-paper-plane"></i> Gửi
                                         </button>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+                            </form>
                         </div>
+                    </div>
+                    <div class="col-lg-6 col-md-5 text-center pt-4">
+                        <img src="/assets/img/1256652.svg" height="280">
                     </div>
                 </div>
             </div>
@@ -31,111 +41,19 @@ export default {
 }
 </script>
 <style>
-button.submit.px-3 {
-    border-color: #ffb701 !important;
-    border-radius: 5px !important;
-    border-width: 1px !important;
-    width: 200px !important;
-    margin: 5px 5px 5px 0px;
-}
-
-
-.fa-plane:before {
-    content: "\f072";
-    margin-right: 8px;
-}
-
-.form-group.d-flex {
-    width: 450px;
-    height: 40px;
-    background-color: white;
-    border-radius: 6px !important;
-    width: 450px;
-    background-color: white;
-}
-.ftco-section-parallax .parallax-img {
-    background-size: cover;
-    background-repeat: no-repeat;
+#follow{
+    background-image: url("/assets/img/pattern_bg.png");
     background-position: center center;
-    position: relative;
-    padding: 4em 0;
-    background: #ffb701;
-
-}
-.heading-section.heading-section-white h2 {
-    font-size: 40px;
-    color: #000;
-}
-.ftco-section-parallax .heading-section-white h2 {
-    font-weight: 400 !important;
-}
-.heading-section.heading-section-white p {
-    color: #000;
-    font-size: 16px;
-}
-.subscribe-form input {
-    border: 1px solid #000 !important;
-}
-.subscribe-form .form-group {
-    position: relative;
-    margin-bottom: 0;
-    -webkit-border-radius: 0;
-    -moz-border-radius: 0;
-    -ms-border-radius: 0;
-    border-radius: 0;
-}
-.subscribe-form .form-group input {
-    background: white !important;
-    border: 1px solid transparent;
-    color: rgba(0, 0, 0, 0.7) !important;
-    font-size: 16px;
-    border-radius: 5px 0px 0px 5px;
-}
-.subscribe-form .form-group .submit {
-    color: #000 !important;
-    background: #ffb701 !important;
-    border-radius: 0px 5px 5px 0px;
-    font-size: 16px;
-    border: 1px solid;
-    padding: 0px 5px !important;
+    background-size: cover;
+    background-color: #1e1e1e;
+    height: 380px;
 }
 
-.subscribe-form .form-group .submit:hover {
-    cursor: pointer;
+.register_form {
+    color: #ececec;
+    padding-top: 50px;
+} 
+.text{
+    color: #fff;
 }
-.subscribe-form .form-group input::-webkit-input-placeholder {
-    /* Chrome/Opera/Safari */
-    color: rgba(0, 0, 0, 0.7) !important;
-}
-
-.subscribe-form .form-group input::-moz-placeholder {
-    /* Firefox 19+ */
-    color: rgba(0, 0, 0, 0.7) !important;
-}
-
-.subscribe-form .form-group input:-ms-input-placeholder {
-    /* IE 10+ */
-    color: rgba(0, 0, 0, 0.7) !important;
-}
-
-.subscribe-form .form-group input:-moz-placeholder {
-    /* Firefox 18- */
-    color: rgba(0, 0, 0, 0.7) !important;
-}
-
-/* iphone X */
-@media (max-width: 400px) {
-    .form-group.d-flex{
-        width: 350px;
-    }
-}
-
-/* Iphone 6 7 8 */
-@media (max-width: 500px ) and ( min-width: 400px) {
-    .form-group.d-flex{
-        width: 390px;
-    }
-}
-
-
 </style>
