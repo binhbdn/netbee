b726a263adeb958113b3bfc742565b71d2f44592
Pham Vu Phuong <phamvuphuong98@gmail.com>
2020-04-17 13:49:25 +0700

    add api code

---
+++ api/app/Http/Controllers/API/TinTucController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Response;
+use JWTAuth;
+use Auth;
+use App\User;
+use Validator;
+use Hash;
+use Carbon\Carbon;
+use App\Mail\Sendmail;
+
+use App\Http\Controllers\NotificationController as notifi;
+
+class TinTucController extends Controller
+{
+
+    public function getTinTucNew(Request $request)
+    {
+        $limit = $request->limit;
+        $datas['tintuc'] = DB::select('CALL GetNews(0,'.$limit.')');
+        if($datas)
+            $data = ['status'=> 200, 'message' => 'Thành công', 'data' => $datas];
+        else
+            $data = ['status'=> 400, 'message' => 'Lỗi get tin', 'data' => null];
+        return response()->json($data);
+    }
+
+    public function getDanhMucTinTuc(Request $request)
+    {
+        $datas = DB::select('CALL GetNewsDanhMuc()');
+        if($datas)
+            $data = ['status'=> 200, 'message' => 'Thành công', 'data' => $datas];
+        else
+            $data = ['status'=> 400, 'message' => 'Lỗi get tin', 'data' => null];
+        return response()->json($data);
+    }
+
+    public function getTinTucCate(Request $request)
+    {
+        $limit = $request->limit;
+        $category = $request->category;
+        $datas['tintuc'] = DB::select('CALL GetNews('.$category.','.$limit.')');
+        if($datas)
+            $data = ['status'=> 200, 'message' => 'Thành công', 'data' => $datas];
+        else
+            $data = ['status'=> 400, 'message' => 'Lỗi get tin', 'data' => null];
+        return response()->json($data);
+    }
+
+
+    public function getDetailTinTuc(Request $request)
+    {
+        $tin = DB::table('news')
+        ->where('id', $request->id)
+        ->first();
+        if($tin)
+            $data = ['status'=> 200, 'message' => 'Thành công', 'data' => $tin];
+        else
+            $data = ['status'=> 400, 'message' => 'Lỗi get tin', 'data' => null];
+        return response()->json($data);
+    }
+
+}
