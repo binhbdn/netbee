af58df9b1acf4116bd687ac330d032d6567ef1be
Pham Vu Phuong <phamvuphuong98@gmail.com>
2020-04-22 18:29:43 +0700

    add login fb

--- api/app/Http/Controllers/API/Auth/UserController.php
+++ api/app/Http/Controllers/API/Auth/UserController.php
@@ -165,170 +165,102 @@ class UserController extends Controller
 
     public function loginfb(Request $request)
     {
-        $id_provider = $request->id;
         $token = $request->token;
-        $checkProvider = DB::table('social_accounts')->where('provider_user_id', $id_provider)->orderby('created_at', 'desc')->first();
+        //check account fb
+        $headers = array('Content-type: application/json');
+        $url = "https://graph.facebook.com/me?fields=id,name,email,picture.type(large)&access_token=".$token;
+        $http = curl_init($url);
+        curl_setopt($http, CURLOPT_HEADER, false);
+        curl_setopt($http, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($http, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($http, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($http, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($http, CURLOPT_VERBOSE, 0);
+        curl_setopt($http, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($http, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($http, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($http);
+        curl_close($http);
+        $inforesult = json_decode($result);
+        $checkProvider = DB::table('social_accounts')->where('provider_user_id', $inforesult->id)->orderby('created_at', 'desc')->first();
         if($checkProvider) {
-            $info = Admin::where('id', $checkProvider->user_id)->first();
-            $customClaims = [
-                'id' => $info->id,
-                'level' => $info->level
-            ];
-            if($jwt_token = JWTAuth::fromUser($info, $customClaims)) {
+            $info = User::where('id', $checkProvider->user_id)->where('block', 0)->first();
+            if($jwt_token = JWTAuth::fromUser($info)) {
                 if($info){
-                    if($info->avatar != null){
-                        $info->avatar = $info->avatar;
-                    }
-                    try {
-                        $input = [
-                            'firebase_token' => $request->firebase_token,
-                            'time_login' => Carbon::now()
-                        ];
-                        DB::table('admins')->where('email', $info->email)->update($input);
-                        $user->firebase_token = $request->firebase_token;
-                    } catch (\Throwable $th) {
-                    }
                     $data = array(
                         'status'  => 200,
                         'message' => 'Đăng nhập thành công',
-                        'user' => $info,
-                        'token' => $jwt_token
+                        'data' => ['user' => Auth::user(), 'token' => $jwt_token],
                     );
                 }
                 else{
                     $data = array(
-                        'status'  =>  400,
-                        'message' => 'Tài khoản của bạn đã bị khóa'
+                        'status'  => 400,
+                        'message' => 'Tài khoản của bạn đã bị khóa',
+                        'data' => null,
                     );
                 }
             }
             else
             {
                 $data = array(
-                    'status'  =>  400,
-                    'message' => 'Sai tên đăng nhập hoặc mật khẩu.'
+                    'status'  => 400,
+                    'message' => 'Lỗi',
+                    'data' => null,
                 );
             }
         } else {
-            //check account fb
-            $headers = array('Content-type: application/json');
-            $url = "https://graph.facebook.com/me?fields=id,name,email,picture.type(large)&access_token=".$token;
-            $http = curl_init($url);
-            curl_setopt($http, CURLOPT_HEADER, false);
-            curl_setopt($http, CURLOPT_CUSTOMREQUEST, "GET");
-            curl_setopt($http, CURLOPT_SSL_VERIFYHOST, 2);
-            curl_setopt($http, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($http, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($http, CURLOPT_VERBOSE, 0);
-            curl_setopt($http, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-            curl_setopt($http, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-            curl_setopt($http, CURLOPT_HTTPHEADER, $headers);
-            $result = curl_exec($http);
-            curl_close($http);
-            $inforesult = json_decode($result);
             //check unique email,phone
-            $checkemail = DB::table('admins')->where('email',$inforesult->email)->count();
+            $checkemail = DB::table('users')->where('email',$inforesult->email)->count();
             $jwt_token = null;
             if($checkemail != 0)
             {
-                $info = Admin::where('email', $inforesult->email)->first();
+                $info = User::where('email', $inforesult->email)->first();
                 if ($jwt_token = JWTAuth::fromUser($info)) {
-                    if($info->avatar != null){
-                        $info->avatar = $info->avatar;
-                    }
-                    try {
-                        $input = [
-                            'firebase_token' => $request->firebase_token,
-                            'time_login' => Carbon::now()
-                        ];
-                        DB::table('admins')->where('email', $inforesult->email)->update($input);
-                        $info->firebase_token = $request->firebase_token;
-                    } catch (\Throwable $th) {
-                    }
                     $data = array(
                         'status'  => 200,
                         'message' => 'Đăng nhập thành công',
-                        'user' => $info,
-                        'token' => $jwt_token
+                        'data' => ['user' => Auth::user(), 'token' => $jwt_token],
                     );
                 }
             }
             else
             {
                 $creatuser = array(
-                        'email' => $inforesult->email,
-                        'password' => bcrypt($inforesult->email),
-                        'fullname' => $inforesult->name,
-                        'username' => $inforesult->name,
-                        'phone' => '',
-                        'avatar' => $inforesult->picture->data->url,
-                        'authen' => '2',
-                        'status' => '0',
-                        'country' => '2',
-                        'level' => '3',
-                        'victory_email' => 1,
-                        'token_email' => md5('devwork'),
-                        'created_at' => date('Y-m-d'),
-                );
+                    'email' => $inforesult->email,
+                    'password' => bcrypt('123456'),
+                    'name' => $inforesult->name,
+                    'phone' => 0,
+                    'status' => '0',
+                    'role' => 1
+                    );
 
-                $users = DB::table('admins')->insertGetId($creatuser);
-                $info = Admin::where('id', $users)->first();
+                $users = DB::table('users')->insertGetId($creatuser);
+                $info = User::where('id', $users)->first();
                 if($users)
                 {
                     DB::table('social_accounts')->insertGetId([
                         'user_id' => $users,
-                        'provider_user_id' => $id_provider,
+                        'provider_user_id' => $inforesult->id,
                         'provider' => 'facebook',
-                        'created_at' => date('Y-m-d'),
-                        'updated_at' => date('Y-m-d')
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
                     ]);
                     if ($jwt_token = JWTAuth::fromUser($info)) {
-                        $list_user = DB::table('admins')->where('level',0)->where('authen',1)->orWhere('level', 4)->get();
-                        $user = [];
-                        foreach ($list_user as $key => $value) {
-                        $user[] = ['user_id' => $value->id];
-                        }
-                        $id_user_action = $users;
-                        $description = 'Vừa có tài khoản hr freelancer mới (chưa xác nhận) được tạo trên hệ thống';
-                        $id_user_info = $user;
-                        $url = 'https://www.devwork.vn/admin/index#/user/list-hr';
-                        $notifi = notifi::create_notification($id_user_action, $description, $url, $id_user_info);
-
-
-                        // $data = ['status'=> 200, 'message' => 'Vui lòng check mail để kích hoạt tài khoản. Nếu trong hộp thư không có vui lòng kiểm tra Tin Quảng Cáo hoặc Spam.'];
-                        if($info->avatar != null){
-                                    $info->avatar = $info->avatar;
-                                }
-                                try {
-                                    $input = [
-                                        'firebase_token' => $request->firebase_token,
-                                        'time_login' => Carbon::now()
-                                    ];
-                                    DB::table('admins')->where('email', $inforesult->email)->update($input);
-                                    $info->firebase_token = $request->firebase_token;
-                                } catch (\Throwable $th) {
-                                }
-                                $data = array(
-                                    'status'  => 200,
-                                    'message' => 'Đăng kí thành công',
-                                    'user' => $info,
-                                    'token' => $jwt_token
-                                );
-                        $mail = $inforesult->email;
-                        $url = 'https://www.devwork.vn/confirm-account/'.$users;
-                        $dataemail = (object)[
-                            'title' => 'Xác nhận tài khoản mới đăng ký',
-                            'content' => 'Cảm ơn '.$inforesult->name.' đã đăng ký tài khoản tại <a href="https://www.devwork.vn" style="color: #ffb701;">DEVWORK.VN</a>',
-                            'body' => 'Để truy cập vào <a href="https://www.devwork.vn" style="color: #ffb701;">DEVWORK.VN</a> vui lòng kích hoạt tài khoản vừa tạo:',
-                            'confirm' => 1,
-                            'url' => $url,
-                        ];
-                        Mail::to($mail)->queue(new Sendmail($dataemail));
+                        $data = array(
+                            'status'  => 200,
+                            'message' => 'Đăng nhập thành công',
+                            'data' => ['user' => Auth::user(), 'token' => $jwt_token],
+                        );
                     }
                 }
                 else
                 {
-                    $data = ['status'=> 400, 'message' => 'Đăng ký thất bại'];
+                    $data = array(
+                        'status'  => 400,
+                        'message' => 'Lỗi',
+                        'data' => null,
+                    );
                 }
             }
         }
---
+++ pages/callback.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <!-- error 404 -->
+                <section class="row flexbox-container">
+                    <div class="col-xl-12 col-md-12 col-12 d-flex justify-content-center">
+                        <div class="card auth-card bg-transparent shadow-none rounded-0 mb-0 w-100">
+                            <div class="card-content">
+                                <div class="card-body text-center">
+                                    <h1>Waiting...</h1>
+                                    <img src="/assets/img/loading.gif" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'no_layout',
+    async mounted() {
+        if(this.$route.query.token) {
+            this.$axios.$get(`loginfb`,{token: this.$route.query.token}).then((response)=>{
+                console.log(response)
+            });
+        }
+        
+    }
+}
+</script>
