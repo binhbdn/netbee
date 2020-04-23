237ea13889320d8e87f29d94b787ae3726d426b8
Pham Vu Phuong <phamvuphuong98@gmail.com>
2020-04-23 12:12:39 +0700

    add scope gg

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,8 @@ module.exports = {
             },
             google: {
                 client_id: '867087688208-tevlsbe2csk6hhlb9sh77kom9mjq6brs.apps.googleusercontent.com',
-                redirect_uri:'http://en-el.devwork.vn/callbackgg'
+                redirect_uri:'http://en-el.devwork.vn/callbackgg',
+                scope: ['openid', 'profile', 'email','picture']
             },
         },
     },
