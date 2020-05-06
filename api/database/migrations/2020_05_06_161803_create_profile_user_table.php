36e0ac002e2c7317ddc98990fcacd08107cc5e19
Thành Nguyễn <devworkvanthanh01@gmail.com>
2020-05-06 17:58:42 +0700

    add-table-cv

---
+++ api/database/migrations/2020_05_06_161803_create_profile_user_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('id_user')->unsigned();
+            $table->string('fullname_profile')->nullable();
+            $table->date('birthday_profile')->nullable();
+            $table->string('maleFemale')->nullable();
+            $table->string('address_profile')->nullable();
+            $table->string('phone_profile')->nullable();
+            $table->string('email_profile')->nullable();
+            $table->string('cmnd_profile')->nullable();
+            $table->string('note_profile')->nullable();
+            $table->string('title_target_profile')->nullable();
+            $table->string('note_target_profile')->nullable();
+            $table->string('skill_communication_profile')->nullable();
+            $table->string('skill_information_profile')->nullable();
+            $table->string('skill_logic_profile')->nullable();
+            $table->string('certificate_profile')->nullable();
+            $table->integer('deleted')->default(0);            
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_user');
+    }
+}
