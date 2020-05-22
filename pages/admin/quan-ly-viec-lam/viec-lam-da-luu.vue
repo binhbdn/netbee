18c6f10485f0b97d336e25eb2f1b93e5aeb844aa
Nhat Minh <dangnhatminh20>
2020-05-22 13:39:31 +0700

    fix: code bad

--- pages/admin/quan-ly-viec-lam/viec-lam-da-luu.vue
+++ pages/admin/quan-ly-viec-lam/viec-lam-da-luu.vue
@@ -5,7 +5,6 @@
 </template>
 <script>
 import Multiselect from "vue-multiselect";
-import "vue-multiselect/dist/vue-multiselect.min.css";
 import ListJobSaved from "~/components/Custom/ListJobSaved";
 import Vue from "vue";
 import moment from "moment";
@@ -13,10 +12,6 @@ export default {
   layout: "admin",
   head: {
     title: "Danh sách việc làm đã lưu",
-    script: [
-      { src: "/app-assets/vendors/js/tables/datatable/pdfmake.min.js" },
-      { src: "/app-assets/vendors/js/tables/datatable/vfs_fonts.js" }
-    ]
   },
   components: {
     Multiselect,
@@ -24,19 +19,8 @@ export default {
   },
   data() {
     return {
-      jobs: [],
-      selected: [],
-      page: 1
+
     };
   },
 };
 </script>
-<style scoped>
-.pagination .page-item.active .page-link {
-  background-color: #ffb701 !important;
-}
-.input_date input {
-  /* max-height: 34px; */
-  padding: 5px;
-}
-</style>
