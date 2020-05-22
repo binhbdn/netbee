9bb060efc73f9db16f32908ba1900319a73b707e
Nhat Minh <dangnhatminh20>
2020-05-22 13:25:27 +0700

    feat: Viec-lam-da-luu.vue, listCard; listJobSaved; fix: saveService, NbJob, narbarAdmin

--- api/app/Services/SaveService.php
+++ api/app/Services/SaveService.php
@@ -2,6 +2,7 @@
 namespace App\Services;
 
 use App\Models\NbJobSave;
+use App\Models\NbJoblist;
 use Carbon\Carbon;
 use Auth;
 use Illuminate\Support\Facades\DB;
@@ -10,9 +11,10 @@ class SaveService extends BaseService {
 
     protected $nbJobSave;
 
-    public function __construct(NbJobSave $nbJobSave)
+    public function __construct(NbJobSave $nbJobSave, NbJoblist $nbJoblist)
     {
         $this->nbJobSave = $nbJobSave;
+        $this->nbJoblist = $nbJoblist;
     }
 
     public function getSaveByJobId($jobId)
@@ -86,4 +88,60 @@ class SaveService extends BaseService {
            ];
         }
     }
+    
+    public function getSaveBySaver($request)
+    {
+        $userId = Auth::user()->id;
+        $perPage = 6;
+        $search = $request->search;
+        $searchCategory = $request->searchCategory;
+        $conditions =[];
+
+        if($searchCategory != ''){
+            $conditions[] = [
+                'type',
+                '=',
+                $searchCategory
+            ];
+        }
+
+        $query = $this->nbJoblist->with('nbJobSave')
+        ->whereHas('nbJobSave', function($q){
+            $q->where('id_saver', Auth::user()->id);
+        })
+        ->with('user')->with('nation');
+
+        if (!empty($conditions)) {
+            $query->where($conditions);
+        }
+        if ($search != '') {
+            $query->where(function ($q) use ($search) {
+                $q->where('title', 'LIKE', '%'.$search.'%');
+            });
+        }
+
+        return $query->orderBy('id', 'DESC')->paginate($perPage);
+
+    }
+
+    // public function deleteJobSave($id)
+    // {
+    //     $delete = $this->nbJobSave->whereId($id)->delete();
+    //     if($delete)
+    //     {
+    //         return [
+    //             'status' => 200,
+    //             'message' => 'Xóa thành công',
+    //             'data' => true
+    //         ];
+    //     }
+    //     return [
+    //         'status' => 400,
+    //         'message' => 'Lỗi',
+    //         'data' => false
+    //     ];
+        
+    // }
+
+
 }
---
+++ pages/admin/quan-ly-viec-lam/viec-lam-da-luu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app-content content">
+    <ListJobSaved />
+  </div>
+</template>
+<script>
+import Multiselect from "vue-multiselect";
+import "vue-multiselect/dist/vue-multiselect.min.css";
+import ListJobSaved from "~/components/Custom/ListJobSaved";
+import Vue from "vue";
+import moment from "moment";
+export default {
+  layout: "admin",
+  head: {
+    title: "Danh sách việc làm đã lưu",
+    script: [
+      { src: "/app-assets/vendors/js/tables/datatable/pdfmake.min.js" },
+      { src: "/app-assets/vendors/js/tables/datatable/vfs_fonts.js" }
+    ]
+  },
+  components: {
+    Multiselect,
+    ListJobSaved
+  },
+  data() {
+    return {
+      jobs: [],
+      selected: [],
+      page: 1
+    };
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+<style scoped>
+.pagination .page-item.active .page-link {
+  background-color: #ffb701 !important;
+}
+.input_date input {
+  /* max-height: 34px; */
+  padding: 5px;
+}
+</style>
