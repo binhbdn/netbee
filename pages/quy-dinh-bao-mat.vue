b9fae67d3a159b87f568130ed924b7d89c48434b
danhnc <danhnc96@gmail.com>
2020-06-18 10:58:45 +0700

    quy dinh bao mat

---
+++ pages/quy-dinh-bao-mat.vue
@@ -0,0 +1,184 @@
+<template>
+  <div>
+    <div class="container">
+      <section>
+        <div class="row">
+          	<div class="col-lg-12 col-12">
+                <div class="card style-card">
+		        	<div class="row">
+			          	<div class="col-md-7">
+			          		<div class="header-ncd">
+				          		<img src="/assets/img/logo-netbee-ncd.png" alt="logo netbee" height="50" width="130">
+								<h1 class="in-hoa" style="margin-top: 5px;"><i class="fa fa-shield-alt"></i> Quy định bảo mật</h1>
+								<div class="col-md-12 mb-3"><p class="line1a"></p></div>
+							</div>
+						</div>
+						<div class="col-md-5" style="text-align: center;">
+							<div class="muc-luc">
+								<h4>Mục lục: </h4>
+								<h5 style="text-align: left; font-weight: 500;"><a href="#mk-bm" data-toggle="pill" data-target="#mk-bm">I. Mật khẩu và bảo mật</a></h5>
+								<h5 style="text-align: left; font-weight: 500;"><a href="#ttdlnd">II. Thông tin và dữ liệu người sử dụng</a></h5>
+								<h5 style="text-align: left; font-weight: 500;"><a href="#tc-cn">III. Truy cập, cập nhật và xóa thông tin</a></h5>
+							</div>
+						</div>
+					</div>
+					<br>
+		            <p>
+		                Netbee tôn trọng những thông tin cá nhân của bạn. Xin hãy đọc và tìm hiểu về những Thỏa
+		                thuận sử dụng và quy định bảo mật thông tin sau đây của chúng tôi. Việc bạn truy cập, đăng
+		                ký, sử dụng trên website Netbee có nghĩa rằng bạn đồng ý và chấp nhận ràng buộc bởi các
+		                điều khoản của bản quy định bảo mật của chúng tôi.</p>
+		            <p>
+	                <h3 id="mk-bm" class="boder-muc">I. Mật khẩu và bảo mật</h3>
+	                <p>Khi bạn đăng ký sử dụng trang web Netbee, bạn sẽ được yêu cầu khởi tạo tài khoản có mật
+	                    khẩu. Chúng tôi khuyến nghị bạn giữ mật khẩu này bảo mật và không được tiết lộ hoặc chia
+	                    sẻ với bất kỳ ai. Nếu bạn biết hoặc nghi ngờ người khác biết mật khẩu của bạn, bạn nên
+	                    thông báo với chúng tôi ngay lập tức bằng cách liên hệ với chúng tôi tới số hotline
+	                    02462900388.</p>
+	                <p>
+	                    Khi Netbee phát hiện bạn có hành vi vi phạm bảo mật hoặc sử dụng không đúng mục đích
+	                    trang web Netbee, chúng tôi có thể yêu cầu bạn thay đổi mật khẩu hoặc chúng tôi có thể
+	                    khóa tài khoản của bạn.</p>
+	                <p>
+	                    Khi bạn tự nguyện tiết lộ thông tin cá nhân của bạn trực tuyến, thông tin đó có thể bị
+	                    người khác thu thập và sử dụng. Trường hợp bạn mất mật khẩu hoặc hoặc sử dụng không đúng
+	                    mục đích trang web Netbee, bạn phải chịu tất cả sự mất mát hoặc thiệt hại phát sinh và
+	                    chịu trách nhiệm sẽ bồi thường hoàn toàn cho Netbee trong trường hợp Netbee có xảy ra
+	                    mất mát hoặc thiệt hại. Đáng tiếc là không có dữ liệu nào truyền trên Internet có thể
+	                    bảo đảm an toàn 100%. Do vậy, mặc dù chúng tôi cố gắng hết sức bảo vệ thông tin cá nhân
+	                    của bạn, Website Netbee.vn có thể không thể bảo đảm hoặc cam kết về tính an toàn của
+	                    thông tin bất kỳ mà bạn chuyển tới chúng tôi hoặc từ dịch vụ trực tuyến của chúng tôi,
+	                    và bạn phải tự chịu rủi ro. Ngay khi chúng tôi nhận được thông tin bạn gửi tới, chúng
+	                    tôi sẽ cố gắng hết sức để bảo đảm an toàn trên hệ thống của chúng tôi.</p>
+	                <p>
+                    <h3 id="ttdlnd" class="boder-muc">II. Thông tin và dữ liệu người sử dụng</h3>
+                    <p>
+                        Bằng việc cung cấp thông tin hoặc sử dụng dịch vụ tại trang web Netbee, bạn đồng ý
+                        để Netbee, các công ty liên kết, đơn vị trực thuộc và các thành viên trực thuộc
+                        Công ty cổ phần thương mại giải pháp Hachinet Việt Nam có thể sử dụng thông tin của
+                        bạn, dù đó là thông tin cá nhân, nhân khẩu học, đều nhằm mục đích điều hành và cải
+                        tiến Website Netbee, tăng cường tiện ích cho người sử dụng hoặc giới thiệu và phân
+                        phối các sản phẩm và dịch vụ của chúng tôi.</p>
+                    <p>
+                        Thông tin chúng tôi có về bạn thông qua quá trình bạn đăng ký hoặc sử dụng Cookies
+                        để lưu và theo dõi thông tin về bạn trong quá trình sử dụng dịch vụ. (Giải nghĩa
+                        Cookies: Cookie là một lượng nhỏ dữ liệu được gửi tới trình duyệt của bạn từ máy chủ
+                        web và được lưu trên đĩa cứng máy tính của bạn. Thông qua cookie:</p>
+                    <p>
+                        a. Chúng tôi nắm được các thông tin từ bạn tại lần cuối cùng của bạn truy cập
+                        website.</p>
+                    <p>
+                        b. Lưu theo yêu cầu của bạn hoặc lưu thông tin truy cập lên máy của bạn để bạn không
+                        phải nhập lại mỗi lần ghé thăm website của chúng tôi.</p>
+                    <p>
+                        c. Cho phép bạn “kiểm tra danh sách” các công việc mà bạn muốn đánh dấu để giữ lại
+                        và xem sau này.)</p>
+                    <p>
+                        Chúng tôi có thể dùng thông tin liên lạc của bạn để gửi cho bạn email hoặc các thông
+                        báo khác về những cập nhật tại website tuyển dụng của Netbee.vn. </p>
+                    <p>Nội dung và tần suất của những thông báo này sẽ thay đổi tuỳ thuộc vào
+                        thông tin mà chúng tôi có về bạn.</p>
+                    <p>
+                        Chúng tôi có thể dùng thông tin thu thập được để thông báo cho bạn về những sản phẩm
+                        và dịch vụ do Website Netbee.vn hay các công ty đối tác cung cấp, hoặc để xin ý
+                        kiến của bạn về các sản phẩm và dịch vụ hiện tại hay những sản phẩm và dịch vụ tiềm
+                        năng mới.</p>
+                    <p>
+                        Chúng tôi có thể tiết lộ thông tin nếu có yêu cầu pháp lý, hay từ một cơ quan chính
+                        phủ hoặc nếu chúng tôi tin rằng hành động đó là cần thiết nhằm: <br/>
+                        a. Tuân theo các yêu cầu pháp lý hoặc chiếu theo quy trình của luật pháp. <br/>
+                        b. Bảo vệ các quyền hay tài sản của Hachinet Việt Nam, hoặc các công ty đối tác.<br/>
+                        c. Ngăn chặn tội phạm hoặc bảo vệ an ninh quốc gia.<br/>
+                        d. Bảo vệ an toàn cá nhân của những người sử dụng hay công chúng.</p>
+                    <p>
+                        Chúng tôi có thể dùng tên bạn, tên hay logo của công ty bạn, hay thông tin khác về
+                        hoặc từ các quảng cáo tuyển dụng hoặc tài khoản xem hồ sơ ứng viên của bạn cho bất
+                        kỳ hay tất cả các mục đích tiếp thị của Công ty CP thương mại giải pháp Hachinet
+                        Việt Nam. Ví dụ, các tên hay logo công ty có thể được dùng trong quảng cáo trên báo,
+                        thư gửi trực tiếp, phương tiện bán hàng, áp phích quảng cáo và các tài liệu khác
+                        liên quan đến Website Netbee hay các tài sản khác của Công ty CP thương mại giải
+                        pháp Hachinet Việt Nam.</p>
+                    <p>
+                        Để là cầu nối giữa Nhà tuyển dụng và Người tìm việc, hồ sơ của Người tìm việc và
+                        Thông tin tuyển dụng của Nhà tuyển dụng được hiển thị trên website Netbee. Chúng
+                        tôi không chịu trách nhiệm nếu một bên thứ 3 cố tình lấy thông tin của người dùng
+                        bất hợp pháp mà chưa được sự cho phép của chúng tôi. Bên thứ 3 sẽ phải chịu hoàn
+                        toàn trách nhiệm trước pháp luật nếu vi phạm quyền sở hữu trí tuệ và xâm phạm tài
+                        sản của chúng tôi (bao gồm tất cả các nội dung trên website Netbee).</p>
+                    <p>
+                        Bất kỳ phản hồi nào bạn gửi đến cho chúng tôi sẽ trở thành tài sản của chúng tôi và
+                        chúng tôi có thể dùng phản hồi đó (chẳng hạn các câu chuyện thành công) cho các mục
+                        đích tiếp thị hoặc liên hệ với bạn để có thêm thông tin.</p>
+                    <p>
+                        <h3 id="tc-cn" class="boder-muc">III. Truy cập, cập nhật và xóa thông tin</h3>
+                    </p>
+                    <p>
+                        Chúng tôi sẽ cung cấp cho bạn các phương tiện đảm bảo thông tin cá nhân của bạn là
+                        chính xác và cập nhật. Bạn có thể hiệu chỉnh hoặc xoá hồ sơ của bạn bất cứ lúc nào
+                        khi bạn đăng nhập vào website Netbee.
+                    </p>
+            	</div>
+          	</div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style type="text/css">
+	.in-hoa{
+		text-transform: uppercase;
+		font-weight: bold;
+	}
+	.color-FFB701{
+		color: #FFB701;
+	}
+	.muc-luc {
+	    border: #ffb7016b solid 2px;
+	    margin: auto;
+	    float: right;
+	    padding: 15px;
+	}
+	.line1a{
+	  width: 260px;
+	  border-top: 2px solid #FFB701;
+	  transform: rotate(-0.22deg);
+	  margin: auto;
+	}
+	.boder-muc{
+		background: #FFB701;
+	    padding: 6px 5px;
+	    max-width: 450px;
+	    border-radius: 5px 50px 5px 5px;
+	}
+	.header-ncd{
+		text-align: center; 
+		padding: 25px 0;
+	}
+	p{
+		padding: 0 10px;
+	}
+	.style-card{
+		padding: 20px 30px; 
+		border-radius: 3px;
+	}
+	@media(max-width: 767px) {
+	    .muc-luc {
+		    display: none;
+		}
+		.header-ncd{
+			text-align: center; 
+			padding: 0;
+			margin-bottom: -40px;
+		}
+		p{
+			padding: 0;
+		}
+		.style-card{
+			padding: 10px; 
+		}
+		.boder-muc {
+		    padding: 5px 15px 5px 5px;
+		}
+	}
+</style>
