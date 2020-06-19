bac159d63cdad365d96715caf38426eb82b6cf80
danhnc <danhnc96@gmail.com>
2020-06-19 17:16:20 +0700

    fix css

--- pages/quy-dinh-bao-mat.vue
+++ pages/quy-dinh-bao-mat.vue
@@ -24,99 +24,58 @@
 					</div>
 					<br>
 		            <p>
-		                Netbee tôn trọng những thông tin cá nhân của bạn. Xin hãy đọc và tìm hiểu về những Thỏa
-		                thuận sử dụng và quy định bảo mật thông tin sau đây của chúng tôi. Việc bạn truy cập, đăng
-		                ký, sử dụng trên website Netbee có nghĩa rằng bạn đồng ý và chấp nhận ràng buộc bởi các
-		                điều khoản của bản quy định bảo mật của chúng tôi.</p>
-		            <p>
+		                Netbee.vn lấy sự hài lòng của người dùng làm kim chỉ nam để phát triển hệ thống, tôn trọng những thông tin cá nhân của bạn. Hãy đọc và tìm hiểu về những Thỏa thuận sử dụng và quy định bảo mật thông tin sau đây của chúng tôi. Việc bạn truy cập, đăng ký, sử dụng trên Website Netbee.vn có nghĩa rằng bạn đồng ý và chấp nhận ràng buộc bởi các điều khoản trong bản quy định bảo mật của chúng tôi.</p>
+
 	                <h3 id="mk-bm" class="boder-muc">I. Mật khẩu và bảo mật</h3>
-	                <p>Khi bạn đăng ký sử dụng trang web Netbee, bạn sẽ được yêu cầu khởi tạo tài khoản có mật
-	                    khẩu. Chúng tôi khuyến nghị bạn giữ mật khẩu này bảo mật và không được tiết lộ hoặc chia
-	                    sẻ với bất kỳ ai. Nếu bạn biết hoặc nghi ngờ người khác biết mật khẩu của bạn, bạn nên
-	                    thông báo với chúng tôi ngay lập tức bằng cách liên hệ với chúng tôi tới số hotline
-	                    02462900388.</p>
 	                <p>
-	                    Khi Netbee phát hiện bạn có hành vi vi phạm bảo mật hoặc sử dụng không đúng mục đích
-	                    trang web Netbee, chúng tôi có thể yêu cầu bạn thay đổi mật khẩu hoặc chúng tôi có thể
-	                    khóa tài khoản của bạn.</p>
+	                	Khi bạn đăng ký sử dụng hệ thống Netbee.vn, bạn sẽ được yêu cầu khởi tạo tài khoản có mật khẩu. Chúng tôi khuyến nghị bạn giữ mật khẩu này bảo mật và không được tiết lộ hoặc chia sẻ với bất kỳ ai. Nếu bạn biết hoặc nghi ngờ người khác biết mật khẩu của bạn, bạn nên thông báo với chúng tôi ngay lập tức bằng cách liên hệ với chúng tôi qua hotline 02462700388.</p>
 	                <p>
-	                    Khi bạn tự nguyện tiết lộ thông tin cá nhân của bạn trực tuyến, thông tin đó có thể bị
-	                    người khác thu thập và sử dụng. Trường hợp bạn mất mật khẩu hoặc hoặc sử dụng không đúng
-	                    mục đích trang web Netbee, bạn phải chịu tất cả sự mất mát hoặc thiệt hại phát sinh và
-	                    chịu trách nhiệm sẽ bồi thường hoàn toàn cho Netbee trong trường hợp Netbee có xảy ra
-	                    mất mát hoặc thiệt hại. Đáng tiếc là không có dữ liệu nào truyền trên Internet có thể
-	                    bảo đảm an toàn 100%. Do vậy, mặc dù chúng tôi cố gắng hết sức bảo vệ thông tin cá nhân
-	                    của bạn, Website Netbee.vn có thể không thể bảo đảm hoặc cam kết về tính an toàn của
-	                    thông tin bất kỳ mà bạn chuyển tới chúng tôi hoặc từ dịch vụ trực tuyến của chúng tôi,
-	                    và bạn phải tự chịu rủi ro. Ngay khi chúng tôi nhận được thông tin bạn gửi tới, chúng
-	                    tôi sẽ cố gắng hết sức để bảo đảm an toàn trên hệ thống của chúng tôi.</p>
-	                <p>
-                    <h3 id="ttdlnd" class="boder-muc">II. Thông tin và dữ liệu người sử dụng</h3>
+	                    Khi Netbee.vn phát hiện bạn có hành vi vi phạm bảo mật hoặc sử dụng không đúng mục đích của Netbee.vn, chúng tôi có thể yêu cầu bạn thay đổi mật khẩu hoặc chúng tôi có thể khóa tài khoản của bạn. Ban sẽ phải chịu sự mất mát hoặc thiệt hại phát sinh và chịu trách nhiệm sẽ bồi thường hoàn toàn cho Netbee.vn trong trường hợp Netbee.vn có xảy ra mất mát hoặc thiệt hại.</p>
+	                <p id="ttdlnd">
+	                    Khi bạn tự nguyện tiết lộ thông tin tài khoản với người khác, thông tin đó có thể bị họ thu thập và sử dụng, Netbee.vn không chịu trách nhiệm đối với những trường hợp này. Đối với trường hợp bạn mất hoặc quên mật khẩu hãy làm mới lại mật khẩu hoặc liên hệ với chúng tôi, đội ngũ hỗ trợ sẽ giúp đỡ bạn một cách nhanh nhất. Khi người dùng sử dụng Internet, các bạn hãy sẵn sàng tâm lý dữ liệu không thể đảm bảo an toàn 100%. Do vậy, Netbee.vn đã cố gắng hết sức để bảo vệ thông tin cá nhân của bạn nhưng có những trường hợp chúng tôi không thể xử lý được. Nên Netbee.vn có thể không bảo đảm hoặc cam kết về tính an toàn của thông tin bất kỳ mà bạn chuyển tới chúng tôi hoặc từ dịch vụ trực tuyến của chúng tôi, và bạn phải tự chịu rủi ro. Ngay khi chúng tôi nhận được thông tin bạn gửi tới, đội ngũ xử lý sẽ làm việc tốt nhất có thể để đảm bảo an toàn trên hệ thống của chúng tôi.</p>
+                    <h3 class="boder-muc">II. Thông tin và dữ liệu người sử dụng</h3>
                     <p>
-                        Bằng việc cung cấp thông tin hoặc sử dụng dịch vụ tại trang web Netbee, bạn đồng ý
-                        để Netbee, các công ty liên kết, đơn vị trực thuộc và các thành viên trực thuộc
-                        Công ty cổ phần thương mại giải pháp Hachinet Việt Nam có thể sử dụng thông tin của
-                        bạn, dù đó là thông tin cá nhân, nhân khẩu học, đều nhằm mục đích điều hành và cải
-                        tiến Website Netbee, tăng cường tiện ích cho người sử dụng hoặc giới thiệu và phân
-                        phối các sản phẩm và dịch vụ của chúng tôi.</p>
+                        Bằng việc cung cấp thông tin hoặc sử dụng dịch vụ tại Website Netbee.vn, bạn đồng ý để Netbee.vn, các công ty liên kết, đơn vị trực thuộc và các thành viên trực thuộc Công ty cổ phần thương mại giải pháp Hachinet Việt Nam có thể sử dụng thông tin của bạn, dù đó là thông tin cá nhân, học vấn và bằng cấp, các mong muốn đó đều nhằm mục đích điều hành và cải tiến Website Netbee.vn, tăng cường tiện ích cho người sử dụng hoặc giới thiệu và phân phối các sản phẩm và dịch vụ của chúng tôi.</p>
                     <p>
-                        Thông tin chúng tôi có về bạn thông qua quá trình bạn đăng ký hoặc sử dụng Cookies
-                        để lưu và theo dõi thông tin về bạn trong quá trình sử dụng dịch vụ. (Giải nghĩa
-                        Cookies: Cookie là một lượng nhỏ dữ liệu được gửi tới trình duyệt của bạn từ máy chủ
-                        web và được lưu trên đĩa cứng máy tính của bạn. Thông qua cookie:</p>
+                        Thông tin chúng tôi có về bạn thông qua quá trình bạn đăng ký, điền thông tin tài khoản, hồ sơ bạn nạp vào các công ty hoặc việc sử dụng Cookies để lưu và theo dõi thông tin về bạn trong quá trình sử dụng dịch vụ. (Giải nghĩa Cookies: Cookie là một lượng nhỏ dữ liệu được gửi tới trình duyệt của bạn từ máy chủ web và được lưu trên đĩa cứng máy tính của bạn. Thông qua Cookie:</p>
                     <p>
-                        a. Chúng tôi nắm được các thông tin từ bạn tại lần cuối cùng của bạn truy cập
-                        website.</p>
+                        a. Chúng tôi nắm được các thông tin từ bạn tại lần cuối cùng bạn truy cập Netbee.vn.</p>
                     <p>
-                        b. Lưu theo yêu cầu của bạn hoặc lưu thông tin truy cập lên máy của bạn để bạn không
-                        phải nhập lại mỗi lần ghé thăm website của chúng tôi.</p>
+                        b. Lưu theo yêu cầu của bạn hoặc lưu thông tin truy cập lên máy của bạn để bạn không phải nhập lại mỗi lần ghé thăm Website của chúng tôi.</p>
                     <p>
-                        c. Cho phép bạn “kiểm tra danh sách” các công việc mà bạn muốn đánh dấu để giữ lại
-                        và xem sau này.)</p>
+                        c. Cho phép bạn “kiểm tra danh sách” các công việc mà bạn muốn đánh dấu để giữ lại và xem sau này.)</p>
                     <p>
-                        Chúng tôi có thể dùng thông tin liên lạc của bạn để gửi cho bạn email hoặc các thông
-                        báo khác về những cập nhật tại website tuyển dụng của Netbee.vn. </p>
-                    <p>Nội dung và tần suất của những thông báo này sẽ thay đổi tuỳ thuộc vào
-                        thông tin mà chúng tôi có về bạn.</p>
+                        Chúng tôi có thể dùng thông tin liên lạc của bạn để gửi email hoặc các thông báo khác về những cập nhật tại Website tuyển dụng của Netbee.vn.</p>
                     <p>
-                        Chúng tôi có thể dùng thông tin thu thập được để thông báo cho bạn về những sản phẩm
-                        và dịch vụ do Website Netbee.vn hay các công ty đối tác cung cấp, hoặc để xin ý
-                        kiến của bạn về các sản phẩm và dịch vụ hiện tại hay những sản phẩm và dịch vụ tiềm
-                        năng mới.</p>
+                    	Nội dung và tần suất của những thông báo này sẽ thay đổi tùy thuộc vào thông tin mà chúng tôi có về bạn.</p>
                     <p>
-                        Chúng tôi có thể tiết lộ thông tin nếu có yêu cầu pháp lý, hay từ một cơ quan chính
-                        phủ hoặc nếu chúng tôi tin rằng hành động đó là cần thiết nhằm: <br/>
-                        a. Tuân theo các yêu cầu pháp lý hoặc chiếu theo quy trình của luật pháp. <br/>
+                        Chúng tôi có thể dùng thông tin thu thập được để thông báo cho bạn về những sản phẩm và dịch vụ do Website Netbee.vn hay các công ty đối tác cung cấp, hoặc để xin ý kiến của bạn về các sản phẩm và dịch vụ hiện tại hay những sản phẩm và dịch vụ tiềm năng mới.</p>
+                    <p>
+                        Chúng tôi có thể tiết lộ thông tin nếu có yêu cầu pháp lý, hay từ một cơ quan chính phủ hoặc nếu chúng tôi tin rằng hành động đó là cần thiết nhằm: <br/>
+                        a. Tuân theo các yêu cầu pháp lý hoặc chiếu theo quy trình của luật pháp.<br/>
                         b. Bảo vệ các quyền hay tài sản của Hachinet Việt Nam, hoặc các công ty đối tác.<br/>
                         c. Ngăn chặn tội phạm hoặc bảo vệ an ninh quốc gia.<br/>
                         d. Bảo vệ an toàn cá nhân của những người sử dụng hay công chúng.</p>
                     <p>
-                        Chúng tôi có thể dùng tên bạn, tên hay logo của công ty bạn, hay thông tin khác về
-                        hoặc từ các quảng cáo tuyển dụng hoặc tài khoản xem hồ sơ ứng viên của bạn cho bất
-                        kỳ hay tất cả các mục đích tiếp thị của Công ty CP thương mại giải pháp Hachinet
-                        Việt Nam. Ví dụ, các tên hay logo công ty có thể được dùng trong quảng cáo trên báo,
-                        thư gửi trực tiếp, phương tiện bán hàng, áp phích quảng cáo và các tài liệu khác
-                        liên quan đến Website Netbee hay các tài sản khác của Công ty CP thương mại giải
-                        pháp Hachinet Việt Nam.</p>
+                       Netbee.vn có thể dùng tên bạn, tên hay logo của công ty bạn, hay thông tin khác về tài khoản cho tất cả các mục đích tiếp thị của Công ty Cổ phần thương mại giải pháp Hachinet Việt Nam. Ví dụ, các tên hay logo công ty có thể được dùng trong quảng cáo trên báo, thư gửi trực tiếp, phương tiện bán hàng, áp phích quảng cáo, video và các tài liệu khác liên quan đến Website Netbee.vn hay các tài sản khác của Công ty Cổ phần thương mại và giải pháp Hachinet Việt Nam.</p>
                     <p>
-                        Để là cầu nối giữa Nhà tuyển dụng và Người tìm việc, hồ sơ của Người tìm việc và
-                        Thông tin tuyển dụng của Nhà tuyển dụng được hiển thị trên website Netbee. Chúng
-                        tôi không chịu trách nhiệm nếu một bên thứ 3 cố tình lấy thông tin của người dùng
-                        bất hợp pháp mà chưa được sự cho phép của chúng tôi. Bên thứ 3 sẽ phải chịu hoàn
-                        toàn trách nhiệm trước pháp luật nếu vi phạm quyền sở hữu trí tuệ và xâm phạm tài
-                        sản của chúng tôi (bao gồm tất cả các nội dung trên website Netbee).</p>
+                        Để là cầu nối giữa Nhà tuyển dụng và Người tìm việc, Người đi du học, hồ sơ của Người tìm việc, Người đi du học và thông tin tuyển dụng của Nhà tuyển dụng được hiển thị trên Website Netbee.vn. Chúng tôi không chịu trách nhiệm nếu một bên thứ 3 cố tình lấy thông tin của người dùng bất hợp pháp mà chưa được sự cho phép của chúng tôi, những thông tin sai lệch của người dùng khi nộp hồ sơ hay đăng tin tuyển dụng. Và chúng tôi sẽ đảm bảo những công ty tuyển dụng là những công ty uy tín, những hồ sơ của Người tìm việc, Người đi du học là những hồ sơ chất lượng, đầy đủ yêu cầu của Nhà tuyển dụng. Những công ty lừa đảo, hay bên thứ 3 sẽ phải chịu hoàn toàn trách nhiệm trước pháp luật nếu vi phạm quyền sở hữu trí tuệ và xâm phạm tài sản của chúng tôi (bao gồm tất cả các nội dung trên Website Netbee.vn).</p>
+                    <p id="tc-cn">
+                        Bất kỳ phản hồi nào của bạn gửi đến chúng tôi sẽ trở thành tài sản của chúng tôi và chúng tôi có thể dùng phản hồi đó (chẳng hạn các câu chuyện thành công) cho các mục đích tiếp thị hoặc liên hệ với bạn để có thêm thông tin.</p>
                     <p>
-                        Bất kỳ phản hồi nào bạn gửi đến cho chúng tôi sẽ trở thành tài sản của chúng tôi và
-                        chúng tôi có thể dùng phản hồi đó (chẳng hạn các câu chuyện thành công) cho các mục
-                        đích tiếp thị hoặc liên hệ với bạn để có thêm thông tin.</p>
-                    <p>
-                        <h3 id="tc-cn" class="boder-muc">III. Truy cập, cập nhật và xóa thông tin</h3>
+                        <h3 class="boder-muc">III. Truy cập, cập nhật và xóa thông tin</h3>
                     </p>
                     <p>
-                        Chúng tôi sẽ cung cấp cho bạn các phương tiện đảm bảo thông tin cá nhân của bạn là
-                        chính xác và cập nhật. Bạn có thể hiệu chỉnh hoặc xoá hồ sơ của bạn bất cứ lúc nào
-                        khi bạn đăng nhập vào website Netbee.
+                        Chúng tôi sẽ cung cấp cho bạn các phương tiện đảm bảo thông tin cá nhân của bạn là chính xác và cập nhật. Bạn có thể hiệu chỉnh hoặc xóa hồ sơ của bạn bất cứ lúc nào khi bạn đăng nhập vào Website Netbee.vn
                     </p>
+                    <br>
+                    <div class="col-md-5 align-self-end" style="float: right; text-align: center;">
+                    	<h6 style="font-weight: bold;">Email: contact@netbee.vn – Hotline: 02462-900-388</h6>
+
+						<h6>Trân trọng!</h6>
+						<br>
+						<h6>Đội ngũ NETBEE</h6>
+                    </div>
             	</div>
           	</div>
         </div>
@@ -157,6 +116,7 @@
 	}
 	p{
 		padding: 0 10px;
+		text-align: justify;
 	}
 	.style-card{
 		padding: 20px 30px; 
