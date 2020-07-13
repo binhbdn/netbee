35725276eddd560d3fbc991ee1a5967beb418119
ngohung1108 <ngohung11081996@gmail.com>
2020-07-13 17:18:00 +0700

    fix-css: #4274

--- components/JobItemAdmin.vue
+++ components/JobItemAdmin.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ecommerce-card border-job-item make-it-sl" style="height:105px;margin-bottom: 15px;border-radius: 3px;background: #fff" :class="job.highlight_job ? 'highlight' : ''">
+    <div class="ecommerce-card border-job-item make-it-sl" style="margin-bottom: 15px;border-radius: 3px;background: #fff" :class="job.highlight_job ? 'highlight' : ''">
         <!-- <div class="card-content">
             <div class="remove-border-right" style="width: 60vh;padding: 5px 25px;">
                 <a class="item-vip-a" :href="`/tin-tuyen-sinh/${job.id}/${ChangeToSlug(job.title)}`" data-toggle="tooltip" data-placement="top" :title="`${job.title}`">{{ job.title }}</a>
@@ -18,31 +18,38 @@
                     <img v-lazy="job.user.avatar != null && job.user.avatar.startsWith('https') ? job.user.avatar : `/uploads/users/avatars/${job.user.avatar}`" height="100%" :alt="`${job.user.avatar}`">
                 </a>
             </div>
-            <div class="remove-border-right" style="width: 550px;">
-                <div class="remove-border-right" style="padding: 5px 0 0 0;width: 550px;">
+            <div class="remove-border-right" style="width: 620px; padding: 0.5rem">
+                <div class="hot-mb" v-if="job.highlight_job == 2"><img src="/assets/img/hot.png"></div>
                     <a class="item-vip-a" :href="`/admin/tin-tuyen-sinh/${job.id}/${ChangeToSlug(job.title)}`" data-toggle="tooltip" data-placement="top" :title="`${job.title}`">[{{job.id}}] {{ job.title }}</a>
-                </div>
-                <div class="item-quantity row" style="height: 21px">
-                    <div class="col-9">
+                <div class="item-quantity row" id="company" style="height: 21px" v-if="job.bonus != 0 && job.bonus != null && $auth.user.role != 3">
+                    <div class="col-md-7 col-12">
                         <a :href="`/cong-ty/${job.user ? job.user.name : job.id_created}`" class="item-company mb-0"><i class="fad fa-building"></i> <span class="company-name" data-toggle="tooltip" data-placement="top" :title="`${job.user.name}`"> {{ job.nb_company.username }}</span></a>
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-5 col-12">
                         <p class="delivery-date" data-toggle="tooltip" data-placement="left" title="Số lượng tuyển" ><i class="fad fa-user-friends"></i> {{job.quantity}}</p>
-                </div>
                     </div>
+                </div>
+                <div class="item-quantity row" id="company" style="height: 21px" v-if="job.bonus != 0 && job.bonus != null && $auth.user.role != 1">
+                    <div class="col-md-7 col-12">
+                        <a :href="`/cong-ty/${job.user ? job.user.name : job.id_created}`" class="item-company mb-0"><i class="fad fa-building"></i> <span class="company-name" data-toggle="tooltip" data-placement="top" :title="`${job.user.name}`"> {{ job.nb_company.username }}</span></a>
+                    </div>
+                    <div class="col-md-5 col-12">
+                        <p class="delivery-date" data-toggle="tooltip" data-placement="left" title="Số lượng tuyển" ><i class="fad fa-user-friends"></i> {{job.quantity}}</p>
+                    </div>
+                </div>
                 <div class="item-quantity row">
-                    <div class="col-9">
+                    <div class="col-md-7 col-12">
                         <p class="quantity-title mb-0" data-toggle="tooltip" data-placement="top" title="địa điểm làm việc"><i class="fa fa-map-marker-alt"></i> {{ job.nation.name }}</p>
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-5 col-12">
                         <p class="delivery-date mb-0" data-toggle="tooltip" data-placement="top" title="hạn nộp hồ sơ"><i class="fa fa-clock"></i> {{ ConvertDate(job.expiration_date) }}</p>
                     </div>
                 </div>
                 <div class="item-price row">
-                    <div class="col-9">
+                    <div class="col-md-7 col-12">
                         <p class="delivery-date mb-0" data-toggle="tooltip" data-placement="top" title="Chi phí" ><label class="title">Chi phí: </label><span :style="[job.highlight_job ? {'color': '#fc205c'} : '']"> {{ FormatPrice(job.subsidy) }} {{ job.currency }}</span></p>
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-5 col-12">
                         <p class="delivery-date mb-0" data-toggle="tooltip" data-placement="top" title="Số lượng tuyển">
                             <span class="badge border-netbee badge-sm" style="width: 100px">
                                 {{ job.type == 1 ? 'Xuất khẩu lao động' : job.type == 2 ? 'Du học sinh' : 'Tu nghiệp sinh' }}
@@ -50,13 +57,19 @@
                         </p>
                     </div>
                 </div>
+                <div class="bonus-block">
+                    <p class="m-0">Tiền thưởng</p>
+                    <h3 class="bonus">
+                        <span style="color: #fc205c">{{FormatPrice(job.bonus)}}{{ job.currency }} / <i class="fad fa-user-friends" title="1 người"></i> </span>
+                    </h3>
+                </div>
             </div>
             <div class="item-options text-center" style="padding: 1rem">
                 <div class="hot" v-if="job.highlight_job == 2"><img src="/assets/img/hot.png"></div>
                 <div class="item-wrapper mt-1">
                     <div class="item-cost mt-1" v-if="job.bonus != 0 && job.bonus != null && $auth.user.role != 1">
                         <p class="m-0">TIỀN THƯỞNG</p>
-                        <h3>
+                        <h3 class="bonus">
                             <span style="color: #fc205c">{{FormatPrice(job.bonus)}}{{ job.currency }} / <i class="fad fa-user-friends" title="1 người"></i> </span>
                         </h3>
                     </div>
@@ -101,7 +114,7 @@ export default {
     grid-template-columns: 0.3fr 4fr 2fr;
 }
 
-.hot{
+.hot, .hot-mb{
     color: #fff;
     text-transform: uppercase;
     z-index: 2;
@@ -110,8 +123,9 @@ export default {
     right: 10px;
     font-weight: 700;
 }
-.hot img {
-    width: 2%;
+.hot img, .hot-mb img {
+    width: 4%;
+    min-width: 35px;
     float: right;
     position: absolute;
     right: 15px;
@@ -131,9 +145,72 @@ export default {
 .item-price .title {
     font-size: 14px;
 }
-@media(max-width: 768px) {
-.remove-border-right {
-    padding: 0px;
+.bonus-block, .hot-mb{
+    display: none;
 }
+
+@media(max-width: 1199px){
+    .remove-border-right{
+        width: 400px !important;
+    }
+    .item-options{
+        padding: 1rem 0;
+    }
+    .bonus{
+        font-size: 1rem;
+    }
+}
+@media(max-width: 991px){
+    .remove-border-right{
+        width: 300px !important;
+    }
+}
+@media(max-width: 767px) and (min-width: 576px){
+    #company{
+        height: 42px !important;
+        position: relative;
+    }
+    .item-options{
+        display: none;
+    }
+    .card-content{
+        grid-template-columns: 1fr 4fr 0 !important;
+    }
+    .remove-border-right{
+        width: auto !important;
+        max-width: 500px !important;
+    }
+    .bonus-block{
+        display: block;
+        position: absolute;
+        top: 58px;
+        left: 420px;
+    }
+    .hot-mb{
+        display: block;
+    }
+    .hot-mb img{
+        right: 15px;
+    }
+}
+@media(max-width:575px){
+    #company{
+        height: 42px !important;
+    }
+    .remove-border-right{
+        max-width: 300px !important;
+        overflow: hidden;
+    }
+    .bonus-block{
+        display: block;
+    }
+    .hot-mb{
+        display: none;
+    }
+}
+@media (max-width:450px) {
+    .remove-border-right{
+        max-width: 200px !important;
+    }
 }
 </style>
