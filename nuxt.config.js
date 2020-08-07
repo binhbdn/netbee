2d452d89e226b2a1ce81761150e87634b0b0f454
Anh Phương <phamvuphuong98@gmail.com>
2020-08-07 10:45:30 +0700

    test fb

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,6 @@ module.exports = {
             { hid: 'stripe', src: '/app-assets/js/core/app.js', defer: true },
             { hid: 'stripe', src: '/app-assets/js/scripts/components.js', defer: true },
             { hid: 'stripe', src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', defer: true },
-            { src: '/assets/js/fb-sdk.js' }
             // { src: '/app-assets/vendors/js/forms/select/select2.full.min.js', },
             // { src: '/app-assets/js/scripts/forms/select/form-select2.js', },
 
