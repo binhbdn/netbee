3bbacc7dfe0dc3eadd202b857c3de640d7cb0014
binhbdn <binh.buidn@gmail.com>
2020-10-02 14:31:27 +0700

    #4685 [All/trangchu] lỗi nhiều kiểu chữ

--- pages/admin/news/index.vue
+++ pages/admin/news/index.vue
@@ -161,7 +161,7 @@
                                                 </tr>
                                             </tbody>
                                         </table>
-                                        <p class="mb-0 text-center p-1 font-italic" v-if="tinTuc.length == 0">Không có dữ liệu nào.</p>
+                                        <p class="mb-0 text-center p-1 font-italic" v-if="tinTuc.length == 0" style="color: red;">Không có dữ liệu nào.</p>
                                     </div>
                                 </div>
                             </div>
--- pages/tin-tuyen-sinh/index.vue
+++ pages/tin-tuyen-sinh/index.vue
@@ -88,7 +88,7 @@
                 <i class="fad fa-business-time"></i> DANH SÁCH ĐƠN HÀNG
               </h4>
               <span v-if="countJob" style="color: red;">Có {{countJob}} kết quả tìm kiếm</span>
-              <span v-else style="color: red;">Không có kết quả phù hợp</span>
+              <span class="font-italic" v-else style="color: red;">Không có kết quả phù hợp</span>
             </div>
             <div class="card-content collapse show">
               <div class="card-body">
