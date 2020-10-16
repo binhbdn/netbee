641a0cd4139fef94f20c6ef5c9a28252adf70836
binhbdn <binh.buidn@gmail.com>
2020-10-16 14:24:56 +0700

    #4487 tạo layout mới cho trang tin tức (news)

---
+++ layouts/news.vue
@@ -0,0 +1,64 @@
+// giống layout default nhưng thay <Banner /> bằng <BannerForNews />
+<template>
+  <div>
+    <HeaderClient />
+    <div class="w-100">
+      <BannerForNews />
+      <nuxt />
+    </div>
+    <Follow />
+    <FooterClient />
+    <GroupIcon />
+  </div>
+</template>
+<script data-ad-client="ca-pub-5263650588670533" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+import HeaderClient from "~/components/HeaderClient";
+import Follow from "~/components/Follow";
+import FooterClient from "~/components/FooterClient";
+import GroupIcon from "~/components/GroupIcon";
+export default {
+  components: {
+    "Follow": () => ({
+      component: new Promise(function (resolve, reject) {
+          setTimeout(function () {
+              resolve(
+                Follow
+              )
+          }, 1000)
+      }),
+      delay: 1000,
+      timeout: 1000
+    }),
+    "GroupIcon": () => ({
+      component: new Promise(function (resolve, reject) {
+          setTimeout(function () {
+              resolve(
+                GroupIcon
+              )
+          }, 1000)
+      }),
+      delay: 1000,
+      timeout: 1000
+    }),
+    "HeaderClient": () => ({
+      component: new Promise(function (resolve, reject) {
+          setTimeout(function () {
+              resolve(
+                HeaderClient
+              )
+          }, 1)
+      }),
+      delay: 1,
+      timeout: 1
+    }),
+    HeaderClient,
+    Follow,
+    FooterClient,
+    GroupIcon
+  },
+  mounted() {
+    this.$i18n.locale = window.localStorage.getItem('lang');
+  }
+};
+</script>
