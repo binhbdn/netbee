884f295243d0868afd67561d3b1315603c9f8314
binhbdn <binh.buidn@gmail.com>
2020-10-29 17:29:54 +0700

    #4568 đổi URL tìm kiếm TinTuyenSinh cho TagList

--- components/TagList.vue
+++ components/TagList.vue
@@ -4,27 +4,27 @@
       <div class="card-content collapse show">
         <div class="card-body" style="text-align: center; padding: 10px">
           	<h5 style="margin:10px 0;"><i class="fa fa-key"></i> TOP TỪ KHÓA</h5>
-          	<a href="/tin-tuyen-sinh/tim-kiem?keyword=&type=1" style="margin:0 2px;">
+			<a href="/tin-tuyen-sinh/xuat-khau-lao-dong?keyword=&type=1" style="margin:0 2px;">
 	          <span class="badge border-netbee badge-sm" style="width: 100px">
 	            Xuất khẩu lao động
 	          </span>
 	        </a>
-	        <a href="/tin-tuyen-sinh/tim-kiem?keyword=&type=2">
+	        <a href="/tin-tuyen-sinh/du-hoc?keyword=&type=2">
 	          <span class="badge border-netbee badge-sm" style="width: 100px">
 	            Du học
 	          </span>
 	        </a>
-	        <a href="/tin-tuyen-sinh/tim-kiem?keyword=&type=3" style="margin:0 2px;">
+	        <a href="/tin-tuyen-sinh/tu-nghiep-sinh?keyword=&type=3" style="margin:0 2px;">
 	          <span class="badge border-netbee badge-sm" style="width: 100px">
 	            Tu nghiệp sinh
 	          </span>
 	        </a>
-	        <a v-for="(item, key) in DataList" :key="key" :href="`/tin-tuyen-sinh/tim-kiem?keyword=${ChangeToSlug(item.tag_name).replace(/\-/gi, '%20')}`" style="margin:0 3px;">
+	        <a v-for="(item, key) in DataList" :key="key" :href="`/tin-tuyen-sinh/${ChangeToSlug(item.tag_name)}?keyword=${ChangeToSlug(item.tag_name).replace(/\-/gi, '%20')}`" style="margin:0 3px;">
               <span class="badge border-netbee badge-sm" style="width: 100px">
                 {{item.tag_name}}
               </span>
             </a>
-	        <a v-for="(item, key) in listNation" :key="key" :href="`/tin-tuyen-sinh/tim-kiem?nation_id=${item.id}`" style="margin:0 3px;">
+	        <a v-for="(item, key) in listNation" :key="key" :href="`/tin-tuyen-sinh/${ChangeToSlug(item.name)}?nation_id=${item.id}`" style="margin:0 3px;">
 	          <span class="badge border-netbee badge-sm" style="width: 100px">
 	            {{item.name}}
 	          </span>
